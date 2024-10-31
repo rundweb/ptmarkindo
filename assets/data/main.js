6f0cc3851bb8fc78d6cb5403064b49e9a34a631c
faldo <pn4066283@gmail.com>
2024-10-31 11:22:21 +0700

    oke done service

--- assets/data/main.js
+++ assets/data/main.js
@@ -3,8 +3,18 @@ const toggleIcon = document.querySelector(".bx-menu-alt-right");
 const listMenu = document.querySelector(".nav-list");
 const listSocial = document.querySelector(".nav-social");
 
-openMenu.addEventListener('click',function () {
-    listMenu.classList.toggle('menu-active');
-    listSocial.classList.toggle('menu-active');
-    toggleIcon.classList.toggle('bx-x')
-})
+openMenu.addEventListener("click", function () {
+  listMenu.classList.toggle("menu-active");
+  listSocial.classList.toggle("menu-active");
+  toggleIcon.classList.toggle("bx-x");
+});
+
+const scrollMenu = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+  if (scrollY > 10) {
+    scrollMenu.classList.add("nav-scroll");
+  } else {
+    scrollMenu.classList.remove("nav-scroll");
+  }
+});
