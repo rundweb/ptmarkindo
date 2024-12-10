6fe5f87e50a941b66520057d77f5719b44ef024e
faldo <pn4066283@gmail.com>
2024-12-10 11:56:37 +0700

    done spices catalog

--- assets/data/main.js
+++ assets/data/main.js
@@ -32,6 +32,210 @@ window.addEventListener("scroll", function () {
   }
 });
 
+var catalogSpices = [
+  {
+    id: 1,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/cloves.png",
+    name: "Cloves",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 2,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/ClovesStem.png",
+    name: "Cloves Stem",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 3,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/Nutmeg.png",
+    name: "Nutmeg",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 4,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/mace.png",
+    name: "Mace",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 5,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/blackpaper.png",
+    name: "Black Pepper",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 6,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/whitepaper.png",
+    name: "White Pepper",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 7,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/cassiastick.png",
+    name: "Cassia Stick",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 8,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/cassiabroken.png",
+    name: "Cassia Broken",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 9,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/bettel.png",
+    name: "Betel Nuts",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 10,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/ginger.png",
+    name: "Ginger",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 11,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/dried-ginger.png",
+    name: "Dried Ginger Slices",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+  {
+    id: 11,
+    label: "Spices",
+    img: "../assets/images/catalog-spices/turmeric.png",
+    name: "Turmeric",
+    desc: "Rich in protein and omega-3, catfish is healthy, flavorful",
+    link: "#",
+  },
+];
+
+var filteredSpices = catalogSpices.filter((items) => items.label === "Spices");
+var filteredCoffee = catalogSpices.filter(
+  (items) => items.label === "Coffee"
+);
+var filteredOils = catalogSpices.filter((items) => items.label === "Oils");
+
+var listAll = catalogSpices
+  .map(
+    (items) => `
+      <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
+          <div class="label">
+              <h1>${items.label}</h1>
+            </div>
+            <div class="catalog-img">
+              <img src=${items.img} alt="" />
+            </div>
+            <div class="catalog-text">
+              <h1>${items.name}</h1>
+              <p>
+                ${items.desc}
+              </p>
+              <div class="catalog-btn">
+                <a href=${items.link} class="btn-more">See More</a>
+                <a href="" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+`
+  )
+  .join("");
+
+var listSpices = filteredSpices
+  .map(
+    (
+      items
+    ) => `<div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
+          <div class="label">
+              <h1>${items.label}</h1>
+            </div>
+            <div class="catalog-img">
+              <img src=${items.img} alt="" />
+            </div>
+            <div class="catalog-text">
+              <h1>${items.name}</h1>
+              <p>
+                ${items.desc}
+              </p>
+              <div class="catalog-btn">
+                <a href=${items.link} class="btn-more">See More</a>
+                <a href="" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>`
+  )
+  .join("");
+var listCofee = filteredCoffee
+  .map(
+    (
+      items
+    ) => `<div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
+          <div class="label">
+              <h1>${items.label}</h1>
+            </div>
+            <div class="catalog-img">
+              <img src=${items.img} alt="" />
+            </div>
+            <div class="catalog-text">
+              <h1>${items.name}</h1>
+              <p>
+                ${items.desc}
+              </p>
+              <div class="catalog-btn">
+                <a href=${items.link} class="btn-more">See More</a>
+                <a href="" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>`
+  )
+  .join("");
+
+  var listOils = filteredOils
+  .map(
+    (
+      items
+    ) => `<div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
+          <div class="label">
+              <h1>${items.label}</h1>
+            </div>
+            <div class="catalog-img">
+              <img src=${items.img} alt="" />
+            </div>
+            <div class="catalog-text">
+              <h1>${items.name}</h1>
+              <p>
+                ${items.desc}
+              </p>
+              <div class="catalog-btn">
+                <a href=${items.link} class="btn-more">See More</a>
+                <a href="" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>`
+  )
+  .join("");
+
 var header = document.getElementById("btn-category");
 var btns = header.getElementsByClassName("btns");
 var catalogList = document.getElementById("catalog-list");
@@ -41,165 +245,13 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
     if (current[0].className == "btns 1 active") {
-      catalogList.innerHTML = `
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-            <div class="label">
-              <h1>Spices</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/catfish.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Catfish (Arius Thalasinnus)</h1>
-              <p>
-                Rich in protein and omega-3, catfish is healthy, flavorful, and
-                versatile.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Coffee Beans</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/snapper.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Red Snapper (Lutjanus Malabaricus)</h1>
-              <p>
-                Rich in protein and omega-3, with a mild flavor and firm
-                texture, perfect for grilling.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Oils</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/pompret.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Pomfret (Pampus Argenteus)</h1>
-              <p>
-                Low in fat, high in protein, and loaded with vitamins and
-                omega-3, ideal for heart health.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Oils</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/ribbon.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Ribbon Fish (Trichiurus Lepturus)</h1>
-              <p>
-                A lean source of protein, packed with selenium and vitamins,
-                known for its soft texture.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>`;
+      catalogList.innerHTML = listAll;
     } else if (current[0].className == "btns 2 active") {
-      catalogList.innerHTML = `
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Spices</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/catfish.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Catfish (Arius Thalasinnus)</h1>
-              <p>
-                Rich in protein and omega-3, catfish is healthy, flavorful, and
-                versatile.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>`;
+      catalogList.innerHTML = listSpices;
     } else if (current[0].className == "btns 3 active") {
-      catalogList.innerHTML = `
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Coffee Beans</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/snapper.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Red Snapper (Lutjanus Malabaricus)</h1>
-              <p>
-                Rich in protein and omega-3, with a mild flavor and firm
-                texture, perfect for grilling.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>`;
+      catalogList.innerHTML = listCofee;
     } else if (current[0].className == "btns 4 active") {
-      catalogList.innerHTML = `
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>oils</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/pompret.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Pomfret (Pampus Argenteus)</h1>
-              <p>
-                Low in fat, high in protein, and loaded with vitamins and
-                omega-3, ideal for heart health.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="catalog-link" data-aos="fade-up" data-aos-duration="1000">
-          <div class="label">
-              <h1>Oils</h1>
-            </div>
-            <div class="catalog-img">
-              <img src="../assets/images/catalog-seafood/ribbon.png" alt="" />
-            </div>
-            <div class="catalog-text">
-              <h1>Ribbon Fish (Trichiurus Lepturus)</h1>
-              <p>
-                A lean source of protein, packed with selenium and vitamins,
-                known for its soft texture.
-              </p>
-              <div class="catalog-btn">
-                <a href="" class="btn-more">See More</a>
-                <a href="" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>`;
+      catalogList.innerHTML = listOils;
     } else {
       console.log(current[0].className + "not pound");
     }
